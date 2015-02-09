0972201103839e1b722b08264f1ba28cdb765033
Ali Rizwan <alir@plumgrid.com>
2015-02-09 23:43:50 +0500

    Added cron job script to delete outdated entities for jenkins.
    Change-Id: Ibede48f994a333c0c183c2a7735876a264d9a20c
    Signed-off-by: Ali Rizwan <alir@plumgrid.com>

---
+++ cron_jobs/jenkins_entity_cleanup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#Script to cleanup running entities for jenkins. Deletes all the instances/disks/snapshots
+#running for longer than the specified time.
+if [[ -z $2 ]]; then
+  user_name="jenkins"
+else
+  user_name="$2"
+fi
+
+time_diff=$1
+instance_list=$(gcloud compute instances list --format=text --sort-by=creationTimestamp --regexp "${user_name}.*")
+#Extract Name and creation time stamp
+instance_names=($(echo "$instance_list" | grep '^name'))
+instance_creationtime=($(echo "$instance_list" | grep creationTimestamp))
+
+for (( i = 1 ; i < ${#instance_names[@]} ; i=i+2 )) do
+  current_date=$(date +"%s")
+  creation_date=$(date -d ${instance_creationtime[$i]} +"%s" )
+  time_difference=$(($current_date - $creation_date))
+  #Modify time as needed
+  if [[ $time_difference -gt $time_diff ]]; then
+    gcloud compute instances delete ${instance_names[$i]} --delete-disks all -q
+  fi
+done
+
+#only get build or run snapshots
+snapshot_list=$(gcloud compute snapshots list --format=text --sort-by=creationTimestamp --regexp "${user_name}.*")
+#Extract Name and creation time stamp
+snapshot_names=($(echo "$snapshot_list" | grep '^name'))
+snapshot_creationtime=($(echo "$snapshot_list" | grep creationTimestamp))
+
+for (( i = 1 ; i < ${#snapshot_names[@]} ; i=i+2 )) do
+  current_date=$(date +"%s")
+  creation_date=$(date -d ${snapshot_creationtime[$i]} +"%s" )
+  time_difference=$(($current_date - $creation_date))
+  #Modify time as needed
+  if [[ $time_difference -gt $time_diff ]]; then
+    gcloud compute snapshots delete ${snapshot_names[$i]} -q
+  fi
+done
+
+#Disks containg name of instances have to excluded from the search in order to find dangling disks
+disk_exclude_list=$(echo "${instance_names[@]} NAME" | sed -e 's/name\: //g' | tr ' ' \| )
+dangling_disks=$(gcloud compute disks list --regexp "${user_name}.*" --sort-by=creationTimestamp | grep -E -v "${disk_exclude_list}" | cut -d ' ' -f 1 )
+dangling_disks=$(echo "$dangling_disks" | tr '\r\n' '|')
+dangling_disks=$(gcloud compute disks list --format text --regexp "(${dangling_disks})")
+dangling_disks_names=($(echo "${dangling_disks}" | grep '^name:'))
+dangling_disks_creationtime=($(echo "${dangling_disks}" | grep creationTimestamp))
+
+for (( i = 1 ; i < ${#dangling_disks_names[@]} ; i=i+2 )) do
+  current_date=$(date +"%s")
+  creation_date=$(date -d ${dangling_disks_creationtime[$i]} +"%s" )
+  time_difference=$(($current_date - $creation_date))
+  #Modify time as needed
+  if [[ $time_difference -gt $time_diff ]]; then
+    gcloud compute disks delete ${dangling_disks_names[$i]} -q
+  fi
+done
