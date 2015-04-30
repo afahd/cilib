7b0b8156e301c10c4433ad1d895b55b5b9eddb83
Ali Rizwan <alir@plumgrid.com>
2015-04-30 19:45:44 +0500

    Moving automaton-init to the build instance.
    builder commit: https://gerrit.plumgrid.com:8443/14306
    
    Change-Id: I33f1435816ea3b68b307062b0bd9202b7a9eb99a
    Ticket: [INFRA-856]
    Signed-off-by: Ali Rizwan <alir@plumgrid.com>

--- aurora/sample_scripts/ExtensiveHALongevityTest05.sh
+++ aurora/sample_scripts/ExtensiveHALongevityTest05.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export EXPORT_FAILURES_DIR=/opt/pg/log/
-sudo /home/plumgrid/work/alps/build/scripts/jenkins/lxc-automaton-longevity-jenkins-init.sh
 cd /home/plumgrid/work/alps/build
 ctest -R UNSTABLE_ExtensiveHALongevityTest05 --output-on-failure -V
--- aurora/sample_scripts/ExtensiveHATest01.sh
+++ aurora/sample_scripts/ExtensiveHATest01.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export EXPORT_FAILURES_DIR=/opt/pg/log/
-sudo /home/plumgrid/work/alps/build/scripts/jenkins/lxc-automaton-jenkins-init.sh
 cd /home/plumgrid/work/alps/build
 ctest -R ExtensiveHATest01 --output-on-failure -V
