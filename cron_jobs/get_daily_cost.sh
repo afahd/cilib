c06cf1a5cfaec5b5a99743f4939d7f02adfc94ef
Irfan Ali Shah <irfans@plumgrid.com>
2015-06-29 13:38:28 +0500

    Cronjob for daily cost usage of all users and all pipelines.
    The cronjob would send out an email with the daily and month usage report listing
    the cost and the time usage of each user and each pipeline.
    Following is a template of the email that would be recieved:
    
    Hi All,
    The cost for aurora instances for the past day/month is as follows:
    
    Cost for all pipelines are as follows:
    
    Cost for pipeline1 = $4.180 Approximate Duration 19:25:0
    Cost for pipeline2 = $.880 Approximate Duration 4:52:0
    Cost for pipeline3 = $0 Approximate Duration 0:0:0
    Cost for pipeline4 = $.220 Approximate Duration 1:1:0
    
    Cost for all users are as follows:
    
    Cost for user1 = $5.280 Approximate Duration 24:0:0
    Cost for user2 = $7.920 Approximate Duration 24:0:0
    Cost for user3 = $.720 Approximate Duration 0:0:0
    Cost for user4 = $10.560 Approximate Duration 48:0:0
    
    The other cronjob scripts would be deleted in the subsequent commit.
    
    Change-Id: I297526d1ac2bdc2c8eb2cdb51e623d022e3e23be
    Signed-off-by: Irfan Ali Shah <irfans@plumgrid.com>

---
+++ cron_jobs/get_daily_cost.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+USER=plumgrid
+export PATH=@CMAKE_BINARY_DIR@/:/opt/pg/scripts:/home/${USER}/google-cloud-sdk/bin:$PATH
+
+TEMP=`getopt -o W:T: --long WORKSPACE:,duration: -n 'get_daily_cost.sh' -- "$@"`
+eval set -- "$TEMP"
+
+while true ; do
+  case "$1" in
+    -W|--WORKSPACE) export WORKSPACE=$2 ; shift 2 ;;
+    -T|--duration) export DURATION=$2 ; shift 2;;
+    --) shift ; break ;;
+    *)
+      echo "Unrecognized option $1"
+       exit 1 ;;
+  esac
+done
+
+if [[ ! -d $WORKSPACE ]]; then
+  mkdir $WORKSPACE
+else
+  rm -rf ${WORKSPACE}*
+fi
+cat /dev/null > ${WORKSPACE}email_template
+
+echo "running cost command"
+aurora cost -U all -p all -T $DURATION -W $WORKSPACE
+# send out email to aurora.internal
+echo "Hi All," >> ${WORKSPACE}email_template
+echo "The cost for aurora instances for the past $DURATION is as follows:" >> ${WORKSPACE}email_template
+echo "" >> ${WORKSPACE}email_template
+cat ${WORKSPACE}cost_final_report >> ${WORKSPACE}email_template
+
+if [[ $DURATION == "day" ]];then
+  subject="Aurora daily cloud resources cost report"
+elif [[ $DURATION == "week" ]];then
+  subject="Aurora weekly cloud resources cost report"
+elif [[ $DURATION == "month" ]];then
+  subject="Aurora monthly cloud resources cost report"
+fi
+
+cat ${WORKSPACE}email_template | mail -s "$subject" "aurora.internal@plumgrid.com" "fmushtaq@plumgrid.com"
