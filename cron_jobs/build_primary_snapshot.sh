511d760acf3aabe5f4664a5063ea70f6d50e834a
Ali Rizwan <alir@plumgrid.com>
2015-07-23 12:57:17 +0500

    Fixed dangling disk deletion not working if list is too long.
    Extra checks aren't required since attached disks cannot be deleted anyway.
    
    Change-Id: I054600365392ef48b5cbbc1482c007d002912928
    Ticket: none
    Signed-off-by: Ali Rizwan <alir@plumgrid.com>

--- cron_jobs/build_primary_snapshot.sh
+++ cron_jobs/build_primary_snapshot.sh
@@ -6,5 +6,5 @@ export USER=plumgrid
 timestamp=$(date +%Y_%m_%d_%H:%M:%S)
 aurora_build_primary_vm_snapshot.sh > ~/primary_snapshot_${timestamp}  2>&1
 
-tail "~/primary_snapshot_${timestamp}" | mail -s "Aurora Snapshot log $timestamp" aurora.internal@plumgrid.com
+tail ~/primary_snapshot_${timestamp} | mail -s "Aurora Snapshot log $timestamp" aurora.internal@plumgrid.com
 
