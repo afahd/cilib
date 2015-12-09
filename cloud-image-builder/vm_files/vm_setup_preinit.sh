35bab6db4f4f92c7b6a397f711b92029a5bb94f7
Muaaz Saleem <muaazs@plumgrid.com>
2015-12-09 03:05:50 -0800

    Fix for snapshot creation
    Before trying to install packages in vm_preinit.sh
    we need to sudo apt-get update to get the latest package
    listings.
    
    Ticket: [INFRA-1048]
    Change-Id: I9be388e0438d36afcc2fdfc2f9a665411bd2bbba
    Signed-off-by: Muaaz Saleem <muaazs@plumgrid.com>
    (cherry picked from commit 69ec18db3bd2c0ab03599fb7f5e0cf2ea5e19f5f)

--- cloud-image-builder/vm_files/vm_setup_preinit.sh
+++ cloud-image-builder/vm_files/vm_setup_preinit.sh
@@ -2,6 +2,7 @@
 set -e
 echo 'Acquire::http { Proxy \"http://10.10.163.162:3142\"; };' > /tmp/02proxy
 sudo mv /tmp/02proxy /etc/apt/apt.conf.d/02proxy
+sudo apt-get update || true
 sudo apt-get install -y btrfs-tools haveged libxml2-dev
 sudo mkfs.btrfs /dev/sdb -L DDISK && echo "LABEL=\"DDISK\" /opt btrfs defaults 0 0" > /tmp/b1 && sudo bash -c "cat /tmp/b1 >> /etc/fstab"
 sudo mount /opt
