a9ea16a5808b787603af63f3dabe7717456e9cce
Fahd Arshad <afahd@plumgrid.com>
2016-10-03 05:44:58 -0700

    Aurora primary snapshot should cleanup if failed
    Change-Id: I4fcb7ebf726cca7d10ba714ef6f2db9a401ce811
    Ticket: [CI-41]
    Signed-off-by: Fahd Arshad <afahd@plumgrid.com>

--- cron_jobs/build_primary_snapshot.sh
+++ cron_jobs/build_primary_snapshot.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -e
 export PATH=/opt/pg/scripts:$PATH
 source gc_helpers.sh
+source aurora_infra_settings.conf
 export USER=plumgrid
 
+function delete_instance() {
+    gcloud compute instances delete $PRIM_INST_NAME --delete-disks all  -q
+}
+
 timestamp=$(date +%Y_%m_%d_%H:%M:%S)
+trap 'delete_instance' EXIT
 aurora_build_primary_vm_snapshot.sh > ~/primary_snapshot_${timestamp}  2>&1
 
 tail ~/primary_snapshot_${timestamp} | mail -s "Aurora Snapshot log $timestamp" aurora.internal@plumgrid.com
