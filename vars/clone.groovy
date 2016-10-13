d3ea02e167a57889a1b892e42908d1cea12141f7
afahd <afahd@plumgrid.com>
2016-10-13 04:10:32 -0700

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -14,11 +14,11 @@ def clone()
    String branch;
    String build;
     string dir=WORKSPACE
-    File currentDirectory = new File(new File(dir+"tesing.txt").getAbsolutePath());
+    File currentDirectory = new File(new File(dir+"/MW/tesing.txt").getAbsolutePath());
     println(currentDirectory.getCanonicalPath());
     println(currentDirectory.getAbsolutePath());
 
-   InputStream input = new FileInputStream(new File(dir+"dependencies.yaml"));
+   InputStream input = new FileInputStream(new File(dir+"/MW/dependencies.yaml"));
    Dependency data = yaml.load(input);
    Set s1 = data.h1.keySet();
    for (int num=0;num<s1.size();num++)
