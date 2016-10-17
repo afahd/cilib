d6230397d3c0073fb08d8dce567a614a920f0e02
Fahd Arshad <afahd@plumgrid.com>
2016-10-17 06:11:06 -0700

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
+import java.nio.charset.StandardCharsets
 
 @Grab(group='org.yaml', module='snakeyaml', version='1.17')
 import org.yaml.snakeyaml.*
