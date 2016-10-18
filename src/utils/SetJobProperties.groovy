b81b6a88ea0f0ff3e8c6872eeab16d03e2a1b69c
Ali Rizwan <alir@plumgrid.com>
2016-10-18 16:43:47 +0500

    Fix typo
    Signed-off-by: Ali Rizwan <alir@plumgrid.com>

--- src/utils/SetJobProperties.groovy
+++ src/utils/SetJobProperties.groovy
@@ -4,7 +4,7 @@ def GetPropertyList(def config) {
 	// Save artifacts for 15 days
 	projectProperties.add(buildDiscarder(logRotator(daysToKeepStr: '15')))
 	// Add properties for gerrit trigger
-	if (config.type == 'review' {
+	if (config.type == 'review') {
 		projectProperties.add(pipelineTriggers([gerrit( gerritProjects: [[branches: [[compareType: 'REG_EXP', pattern: '.*']],
 		 compareType: 'PLAIN', pattern: '$env.GERRIT_PROJECT', filePaths: [[compareType: 'REG_EXP', pattern: '${config.trigger_path}']]]],
 		 triggerOnEvents: [commentAddedContains('.*runpipeline: ${config.name}.*'), patchsetCreated(excludeDrafts: true, excludeNoCodeChange: true, excludeTrivialRebase: false)])]))
