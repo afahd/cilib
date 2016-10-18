6b3f65609851df21b4a00a1643597b2e905e9e6d
Ali Rizwan <alir@plumgrid.com>
2016-10-18 16:39:08 +0500

    Updating
    Signed-off-by: Ali Rizwan <alir@plumgrid.com>

---
+++ src/utils/SetJobProperties.groovy
@@ -0,0 +1,18 @@
+def GetPropertyList(def config) {
+	// Define the constant properties
+	def projectProperties = []
+	// Save artifacts for 15 days
+	projectProperties.add(buildDiscarder(logRotator(daysToKeepStr: '15')))
+	// Add properties for gerrit trigger
+	if (config.type == 'review' {
+		projectProperties.add(pipelineTriggers([gerrit( gerritProjects: [[branches: [[compareType: 'REG_EXP', pattern: '.*']],
+		 compareType: 'PLAIN', pattern: '$env.GERRIT_PROJECT', filePaths: [[compareType: 'REG_EXP', pattern: '${config.trigger_path}']]]],
+		 triggerOnEvents: [commentAddedContains('.*runpipeline: ${config.name}.*'), patchsetCreated(excludeDrafts: true, excludeNoCodeChange: true, excludeTrivialRebase: false)])]))
+	}
+	return projectProperties
+}
+
+
+SetJobProperties(def config) {
+	properties(GetPropertyList(config))
+}
