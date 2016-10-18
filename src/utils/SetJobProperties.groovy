147c2cf0e570d88c47746057f3510054a5a80275
Ali Rizwan <alir@plumgrid.com>
2016-10-18 20:59:30 +0500

    [WIP]: Adding aurora wrapper
    Change-Id: Ibf7644ab702b18e97c4ce2ebcd760645a58daae1
    Ticket: none
    Signed-off-by: Ali Rizwan <alir@plumgrid.com>

---
+++ src/utils/SetJobProperties.groovy
@@ -0,0 +1,18 @@
+package utils;
+
+def GetPropertyList(def conf) {
+	// Define the constant properties
+	def projectProperties = []
+	// Save artifacts for 15 days
+	projectProperties.add(buildDiscarder(logRotator(daysToKeepStr: '15')))
+	// Add properties for gerrit trigger
+	if (conf.type == 'review') {
+		projectProperties.add(pipelineTriggers([gerrit( gerritProjects: [[branches: [[compareType: 'REG_EXP', pattern: '.*']],
+		 compareType: 'PLAIN', pattern: '$env.GERRIT_PROJECT', filePaths: [[compareType: 'REG_EXP', pattern: '${conf.trigger_path}']]]],
+		 triggerOnEvents: [commentAddedContains('.*runpipeline: ${conf.name}.*'), patchsetCreated(excludeDrafts: true, excludeNoCodeChange: true, excludeTrivialRebase: false)])]))
+	}
+	return projectProperties;
+}
+
+
+return this
---
+++ vars/aurora.groovy
@@ -0,0 +1,22 @@
+#!groovy
+
+def call(body) {
+
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+
+  def a = new utils.GetPropertyList()
+  properties(a.GetPropertyList(config))
+
+  node {
+    stage 'build'
+    timeout(config.timeout) {
+
+    echo "Starting aurora build, project:$env.GERRIT_PROJECT, branch:$env.GERRIT_BRANCH refspec:$env.GERRIT_REFSPEC"
+    }
+  }
+}
+
