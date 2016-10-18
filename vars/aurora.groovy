165239777e283998ad5ce6679f3710ad84d8e725
alirizw <alir@plumgrid.com>
2016-10-18 15:02:24 +0500

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -10,10 +10,7 @@ def call(body) {
     stage 'build'
     timeout(config.timeout) {
     
-    echo 'Starting aurora build, project:$env.GERRIT_PROJECT, branch:$env.GERRIT_BRANCH refspec:$env.GERRIT_REFSPEC'
-    sh 'aurora build -p $GERRIT_PROJECT' -b '$GERRIT_BRANCH' -r '$GERRIT_REFSPEC' -T 'automaton'
-      
-    
-  }
+    echo "Starting aurora build, project:$env.GERRIT_PROJECT, branch:$env.GERRIT_BRANCH refspec:$env.GERRIT_REFSPEC"
+    }
   }
 }
