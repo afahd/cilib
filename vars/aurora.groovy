58aecb569827432dad2038f5114727d075b08cde
alirizw <alir@plumgrid.com>
2016-10-18 14:57:39 +0500

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -6,25 +6,12 @@ def call(body) {
   body.delegate = config
   body()
   
-  if (config.type == 'review'){
-    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '15', numToKeepStr: '')),
-    <object of type com.suryagaddipati.jenkins.SlaveUtilizationProperty>,
-    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false],
-    [$class: 'ThrottleJobProperty', categories: [], limitOneJobWithMatchingParams: false, maxConcurrentPerNode: 0,
-      maxConcurrentTotal: 0, paramsToUseForLimit: '', throttleEnabled: false, throttleOption: 'project'],
-    pipelineTriggers([gerrit(customUrl: '', gerritProjects: [[branches: [[compareType: 'ANT', pattern: '*']],
-      compareType: 'PLAIN', disableStrictForbiddenFileVerification: false,
-      pattern: 'jenkinstest', filePaths: [[compareType: 'REG_EXP', pattern: '$config.trigger_path']]]],
-      serverName: 'Gerrit Server',
-      triggerOnEvents: [patchsetCreated(excludeDrafts: true, excludeNoCodeChange: true, excludeTrivialRebase: false),
-       commentAddedContains('.*runpipeline: $config.name.*')])])])
-  }
-  
   node {
     stage 'build'
     timeout(config.timeout) {
     
-      sh 'aurora build -p $GERRIT_PROJECT' -b '$GERRIT_BRANCH' -r '$GERRIT_REFSPEC' -T 'automaton'
+    echo 'Starting aurora build, project:$env.GERRIT_PROJECT, branch:$env.GERRIT_BRANCH'
+    sh 'aurora build -p $GERRIT_PROJECT' -b '$GERRIT_BRANCH' -r '$GERRIT_REFSPEC' -T 'automaton'
       
     
   }
