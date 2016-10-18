c94f334fa6e55cac16d3039ee8abf6f5b10554cb
alirizw <alir@plumgrid.com>
2016-10-18 15:07:27 +0500

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -8,6 +8,7 @@ def call(body) {
   
   node {
     stage 'build'
+    clone()
     timeout(config.timeout) {
     
     echo "Starting aurora build, project:$env.GERRIT_PROJECT, branch:$env.GERRIT_BRANCH refspec:$env.GERRIT_REFSPEC"
