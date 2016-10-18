8f3431961426404b137b0a20ea81f0b3b49e5feb
Fahd Arshad <afahd@plumgrid.com>
2016-10-18 07:28:41 -0700

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,54 +1,48 @@
 import java.nio.charset.StandardCharsets
 
-//@Grab(group='org.yaml', module='snakeyaml', version='1.17')
-//import org.yaml.snakeyaml.*
+@Grab(group='org.yaml', module='snakeyaml', version='1.17')
+import org.yaml.snakeyaml.*
 
 @NonCPS
-def loop()
-{   String project;
-    String location;
-    String branch;
-    String build;
-    println("here")
-    //Set s1 = data.keySet()
-    //println(s1.size())
- 
-    for (int num=0;num<2;num++)
-    {
-        project = s1.toList()[num];
-        //println(project)
-        location = data.get(project)['location'];
-        branch = data.get(project)['branch'];
-        build = data.get(project)['build'];
-        String git_url = location.replace("[","").replace("]","")
-        //println(git_url)
-        String git_branch = branch.replace("[","").replace("]","")
-        //println(git_branch)
-        echo "Cloning dependencies for $project "
-        //clonning(git_branch,git_url)
-        //git branch: 'git_branch', url: "git_url"
-    }
+List get_project(String s2)
+{
+    Yaml yaml = new Yaml();
+    Map<String, Object> yaml_map = new HashMap<String, Object>(yaml.load(s2));
+    List keys = new ArrayList(yaml_map.keySet());
+    return keys
 }
 
+@NonCPS
+String get_location(String proj, String input)
+{
+    Yaml yaml = new Yaml();
+    Map<String, Object> yaml_map = new HashMap<String, Object>(yaml.load(input));
+    String git_url = yaml_map.get(proj)['location']
+    return git_url.replace("[","").replace("]","")
+
+}
+
+@NonCPS
+String get_branch(String proj, String input)
+{
+    Yaml yaml = new Yaml();
+    Map<String, Object> yaml_map = new HashMap<String, Object>(yaml.load(input));
+    String git_branch = yaml_map.get(proj)['branch']
+    return git_branch.replace("[","").replace("]","")
+
+}    
 
 def clone()
 {
     String input2 = readFile 'dependencies.yaml'
-    println(input2)
-    //InputStream input = new ByteArrayInputStream(input2.getBytes(StandardCharsets.UTF_8));
-    //println(input)
-   
-    //Yaml yaml = new Yaml();
-    //Map<String, Object> yaml_map = new HashMap<String, Object>(yaml.load(input2));
-    println("this is after")
-    //println(yaml_map);
-     //Set s1 = yaml_map.keySet()
-    //println(s1.size())
-    for (int i =0; i<2;i++)
+   List l1 = get_project(input2)
+    for(int i=0; i<l1.size();i++)
     {
-        println(i)
+        location = get_location(l1.get(i),input2)
+        branch = get_branch(l1.get(i),input2)
+        echo "Cloning dependencies for $project "
+        git branch: 'git_branch', url: "git_url"
     }
-    //git_clone(yaml_map) 
 }
 
 return this;
