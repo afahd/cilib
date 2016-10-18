5f0b335f2578d677c0cdb0fb1e22e80b1e983456
Ali Rizwan <alir@plumgrid.com>
2016-10-18 17:39:43 +0500

    teststsety y
    Signed-off-by: Ali Rizwan <alir@plumgrid.com>

--- src/utils/GetPropertyList.groovy
+++ src/utils/GetPropertyList.groovy
@@ -7,7 +7,7 @@ def GetPropertyList(java.util.LinkedHashMap conf) {
 	projectProperties.add(buildDiscarder(logRotator(daysToKeepStr: '15')))
 	// Add properties for gerrit trigger
 	if (conf.type == 'review') {
-		projectProperties.add(pipelineTriggers([gerrit( gerritProjects: [[branches: [[compareType: 'REG_EXP', pattern: '.*']],
+		projectProperties.add(pipelineTriggers([gerritProjectsit( gerritProjects: [[branches: [[compareType: 'REG_EXP', pattern: '.*']],
 		 compareType: 'PLAIN', pattern: '$env.GERRIT_PROJECT', filePaths: [[compareType: 'REG_EXP', pattern: '${conf.trigger_path}']]]],
 		 triggerOnEvents: [commentAddedContains('.*runpipeline: ${conf.name}.*'), patchsetCreated(excludeDrafts: true, excludeNoCodeChange: true, excludeTrivialRebase: false)])]))
 	}
@@ -16,7 +16,7 @@ def GetPropertyList(java.util.LinkedHashMap conf) {
 
 
 def test() {
-	println "test"
+	println "test";
 }
 
-return this
+return this;
