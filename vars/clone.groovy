d35faf2837c5d6b3a6b2823e6dd0ecca1297afb9
Fahd Arshad <afahd@plumgrid.com>
2016-10-19 04:30:40 -0700

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,7 @@
-import java.nio.charset.StandardCharsets
 
+package utils;
+
+import java.nio.charset.StandardCharsets
 // Importing external jar file with the help of grapes
 @Grab(group='org.yaml', module='snakeyaml', version='1.17')
 import org.yaml.snakeyaml.*
@@ -20,7 +22,7 @@ String getLocation(String proj, String dep_file)
     Yaml yaml = new Yaml();
     Map<String, Object> yaml_map = new HashMap<String, Object>(yaml.load(dep_file));
     String git_url = yaml_map.get(proj)['location']
-    // Removing special characters [ and ] 
+    // Removing special characters [ and ]
     return git_url.replace("[","").replace("]","")
 }
 
@@ -40,29 +42,29 @@ def checkDependency()
 
 List cloneDependencies(String repo)
 {
-        // Built in readFile for groovy that read a file and returns a string
-        dir ("$repo")
+    // Built in readFile for groovy that read a file and returns a string
+    dir ("$repo")
+    {
+        if (checkDependency())
         {
-            if (checkDependency())
+            String dep_input = readFile "dependencies.yaml"
+            List project_list = getProjects(dep_input)
+            for(int i=0; i<project_list.size();i++)
             {
-                String dep_input = readFile "dependencies.yaml"
-                List project_list = getProjects(dep_input)
-                for(int i=0; i<project_list.size();i++)
+                def project_name = project_list.get(i)
+                location = getLocation(project_name,dep_input)
+                branch = getBranch(project_name,dep_input)
+                echo "Cloning dependencies from $location "
+                sh "mkdir -p $WORKSPACE/$project_name;"
+                dir ("$WORKSPACE/$project_name")
                 {
-                    def project_name = project_list.get(i)
-                    location = getLocation(project_name,dep_input)
-                    branch = getBranch(project_name,dep_input)
-                    echo "Cloning dependencies from $location "
-                    sh "mkdir -p $WORKSPACE/$project_name;"
-                    dir ("$WORKSPACE/$project_name")
-                    {
-                        // built in git function to clone a repository
-                        git branch: "$branch", url: "$location"
-                    }
+                    // built in git function to clone a repository
+                    git branch: "$branch", url: "$location"
                 }
-                return project_list
             }
+            return project_list
         }
+    }
 }
 
 def cloneProject(String repo_name, String repo_url)
@@ -72,10 +74,10 @@ def cloneProject(String repo_name, String repo_url)
     {
         git branch: "master", url: "$repo_url"
     }
-    
+
     List projects = []
     projects.push("$repo_name")
-    
+
     while(!projects.isEmpty())
     {
         List dependent_list = cloneDependencies(projects.get(0));
@@ -85,7 +87,6 @@ def cloneProject(String repo_name, String repo_url)
             projects.addAll(dependent_list)
         }
     }
-      
 }
 
 return this;
