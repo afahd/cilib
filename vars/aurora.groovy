de760aa8bf2ca1bba9ccba12254a83b6be499a2f
Fahd Arshad <afahd@plumgrid.com>
2016-10-24 23:40:38 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -7,13 +7,12 @@ def call(body) {
   body.delegate = config
   body()
 
-
-  def a = new utils.GetPropertyList()
-  properties(a.GetPropertyList(config))
+  //def a = new utils.GetPropertyList()
+  //properties(a.GetPropertyList(config))
 
   node {
     stage 'build'
-    timeout(config.timeout) {
+    //timeout(config.timeout) {
 
     echo "Starting aurora build, project:$env.GERRIT_PROJECT, branch:$env.GERRIT_BRANCH refspec:$env.GERRIT_REFSPEC"
     }
