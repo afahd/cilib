8799beac20238268e4ca3d77cae6ac03bd0c84e6
Fahd Arshad <afahd@plumgrid.com>
2016-10-25 03:45:06 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -12,7 +12,9 @@ def call(body) {
     dir('andromeda') 
     {
       git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
+      sh 'git clone ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
     }  
+    
     sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
     sh 'aurora --help'
    
