61a6f7148b71f41a7ff1f24fbbfcdaff841ffa88
Fahd Arshad <afahd@plumgrid.com>
2016-10-25 09:10:48 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -11,18 +11,18 @@ def call(body) {
     stage 'clone'
     dir('andromeda') 
     {
-      //git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
-      //sh "git fetch ssh://afahd@gerrit.plumgrid.com:29418/andromeda refs/changes/80/26680/6 && git checkout FETCH_HEAD" 
+      git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
+      sh "git fetch ssh://afahd@gerrit.plumgrid.com:29418/andromeda refs/changes/80/26680/6 && git checkout FETCH_HEAD" 
      
     } 
     
     withEnv(["PATH=/opt/plumgrid/google-cloud-sdk/bin/:/opt/pg/scripts:$PATH"]) 
     {
-      //sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
+      sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
       echo "$args.name"
       stage 'build'
       echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
-      //sh "aurora build -p corelib -b master -t core"
+      sh "aurora build -p corelib -b master -t $BUILD_TAG"
       def string_out = readFile('logs/build_id')
       def build_id = string_out.replace("BUILD-ID=","")
       stage 'tests'
