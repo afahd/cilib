7e535cd9c1aa215bb6fb47cad008229314c9e483
Fahd Arshad <afahd@plumgrid.com>
2016-10-25 00:24:48 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -8,6 +8,7 @@ def call(body) {
   body()
   
   node('local-node') {
+    git'ssh://gerrit.plumgrid.com:29418/andromeda'
     echo "$args.name"
     stage 'build'
     echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
