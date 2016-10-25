a7d315d2cb39e03edf739fee280cb7ff23f580eb
Fahd Arshad <afahd@plumgrid.com>
2016-10-25 00:17:04 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -7,6 +7,7 @@ def call(body) {
   body.delegate = config
   body()
   node('local-node') {
+    echo "config.name"
     stage 'build'
     echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
   }
