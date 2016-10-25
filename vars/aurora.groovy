08c71be503284542bf4c012f1c3cb6a841bdf9f0
Fahd Arshad <afahd@plumgrid.com>
2016-10-25 07:27:08 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -11,17 +11,19 @@ def call(body) {
     
     dir('andromeda') 
     {
-      git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
+      //git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
     } 
     
-    withEnv(["PATH=/opt/plumgrid/google-cloud-sdk/bin/:/opt/pg/scripts:$PATH"]) {
-      echo "$PATH"
-      sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
+    withEnv(["PATH=/opt/plumgrid/google-cloud-sdk/bin/:/opt/pg/scripts:$PATH"]) 
+    {
+      //sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
       echo "$args.name"
       stage 'build'
-      sh "which gcloud"
       echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
-      sh "aurora build -p corelib -b master -t testing"
+      //sh "aurora build -p corelib -b master -t core"
+      def build_id = readfile('logs/build_id')
+      echo "$build_id"
+      
     }
      
   }
