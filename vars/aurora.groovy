5247f2c643d9a4945b2d1940f5ebcedbe35994b6
Fahd Arshad <afahd@plumgrid.com>
2016-10-25 00:56:07 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -14,6 +14,7 @@ def call(body) {
       git branch: 'master', url: 'ssh://gerrit.plumgrid.com:29418/andromeda'
     }  
     sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
+    sh 'aurora --help'
    
     echo "$args.name"
     stage 'build'
