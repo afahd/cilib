97edd55b1e6278e1898d7be2a6d7437874d7ae66
Fahd Arshad <afahd@plumgrid.com>
2016-10-26 04:29:12 -0700

    Update GetPropertyList.groovy

--- src/utils/GetPropertyList.groovy
+++ src/utils/GetPropertyList.groovy
@@ -6,9 +6,10 @@ def GetPropertyList(def conf) {
 	// Save artifacts for 15 days
 	projectProperties.add(buildDiscarder(logRotator(daysToKeepStr: '15')))
 	// Add properties for gerrit trigger
-	if (conf.type == 'review') {
-		projectProperties.add(pipelineTriggers([gerrit( gerritProjects: [[branches: [[compareType: 'REG_EXP', pattern: '.*']],
-		 compareType: 'PLAIN', pattern: "$env.GERRIT_PROJECT", filePaths: [[compareType: 'REG_EXP', pattern: "${conf.trigger_path}"]]]],
+	if (conf.type == 'review')
+	{
+		projectProperties.add(pipelineTriggers([gerrit( gerritProjects: [[branches: [[compareType: 'PLAIN', pattern: 'GERRIT_BRANCH']],
+		 compareType: 'PLAIN', pattern: "$GERRIT_PROJECT", filePaths: [[compareType: 'REG_EXP', pattern: "${conf.trigger_path}"]]]],
 		 triggerOnEvents: [commentAddedContains(".*runpipeline: ${conf.name}.*"), patchsetCreated(excludeDrafts: true, excludeNoCodeChange: true, excludeTrivialRebase: false)])]))
 	}
         // TODO: Create properties for periodic pipelines
