57d234b6607d9338f20ed625b2f3dbb32a5ff2d1
Fahd Arshad <afahd@plumgrid.com>
2016-10-26 04:36:40 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -7,30 +7,40 @@ def call(body) {
   body.delegate = args
   body()
   
+  if (config.trigger_path != null)
+  {
+    def a = new utils.GetPropertyList()
+    properties(a.GetPropertyList(config))
+  }
+  
   node('gcloud-slave') {
     
-    stage 'clone'
-    dir('andromeda') 
+    timeout(config.timeout) 
     {
-      git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
-    } 
-    
-    withEnv(["PATH=/opt/plumgrid/google-cloud-sdk/bin/:$WORKSPACE/andromeda/gcloud/build:/opt/pg/scripts:$PATH"]) 
-    {
-      sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..;'
       
-      stage 'build'
-      echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
-      sh "aurora build -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $JOB_BASE_NAME+$BUILD_NUMBER -r $GERRIT_REFSPEC"
-      def string_out = readFile('logs/build_id')
-      def build_id = string_out.replace("BUILD-ID=","")
-      
-      stage 'test'
-      sh "aurora test -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.ctest_tag -n $args.num_instances -i $args.iterations -A $args.test_args -l $build_id "
+      stage 'clone'
+      dir('andromeda') 
+      {
+        git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
+      } 
+
+      withEnv(["PATH=/opt/plumgrid/google-cloud-sdk/bin/:$WORKSPACE/andromeda/gcloud/build:/opt/pg/scripts:$PATH"]) 
+      {
+        sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..;'
+
+        stage 'build'
+        echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
+        sh "aurora build -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $JOB_BASE_NAME+$BUILD_NUMBER -r $GERRIT_REFSPEC"
+        def string_out = readFile('logs/build_id')
+        def build_id = string_out.replace("BUILD-ID=","")
+
+        stage 'test'
+        sh "aurora test -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.ctest_tag -n $args.num_instances -i $args.iterations -A $args.test_args -l $build_id "
+      }
+
+      archiveArtifacts "$args.archive"
+      step([$class: 'WsCleanup'])
     }
-    
-    archiveArtifacts "$args.archive"
-    step([$class: 'WsCleanup'])
      
   }
 }
