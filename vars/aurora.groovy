73592c2d7bb656c9601b3d054a042c3d734b3de7
Fahd Arshad <afahd@plumgrid.com>
2016-10-26 01:12:47 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -8,22 +8,22 @@ def call(body) {
   body()
   
   node('gcloud-slave') {
-    deleteDir()
+    //deleteDir()
     stage 'clone'
     dir('andromeda') 
     {
-      git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
-      sh "git fetch ssh://afahd@gerrit.plumgrid.com:29418/andromeda refs/changes/80/26680/6 && git checkout FETCH_HEAD" 
+      //git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
+      //sh "git fetch ssh://afahd@gerrit.plumgrid.com:29418/andromeda refs/changes/80/26680/6 && git checkout FETCH_HEAD" 
      
     } 
     
     withEnv(["PATH=/opt/plumgrid/google-cloud-sdk/bin/:/opt/pg/scripts:$PATH"]) 
     {
-      sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
+      //sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..; make install;'
       echo "$args.name"
       stage 'build'
       echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
-      sh "aurora build -p corelib -b master -t $BUILD_TAG"
+      //sh "aurora build -p corelib -b master -t $BUILD_TAG"
       def string_out = readFile('logs/build_id')
       def build_id = string_out.replace("BUILD-ID=","")
       stage 'tests'
