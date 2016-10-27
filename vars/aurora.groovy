9913bdc97cf777414056ad07f6030045080556a7
Fahd Arshad <afahd@plumgrid.com>
2016-10-26 23:13:32 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -35,9 +35,9 @@ def call(body) {
         stage 'build'
         echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
        // sh "aurora build -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $JOB_BASE_NAME+$BUILD_NUMBER -r $GERRIT_REFSPEC"
-        def string_out = readFile('logs/build_id')
-        def build_id = string_out.replace("BUILD-ID=","")
-
+        //def string_out = readFile('logs/build_id')
+        //def build_id = string_out.replace("BUILD-ID=","")
+        def build_id = "123"
         stage 'test'
         
         String testing = "aurora test -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.ctest_tag -n $args.num_instances -i $iter -A $args.test_args -l $build_id"
