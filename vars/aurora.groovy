d687b241a6bc77db8544b774037e09678913d515
Fahd Arshad <afahd@plumgrid.com>
2016-10-26 23:34:23 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -46,22 +46,24 @@ def call(body) {
 
         stage 'build'
         echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
-        sh "aurora build -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $JOB_BASE_NAME+$BUILD_NUMBER -r $GERRIT_REFSPEC"
-        def string_out = readFile('logs/build_id')
-        def build_id = string_out.replace("BUILD-ID=","")
-        
-        if (build_id == null)
+        //sh "aurora build -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $JOB_BASE_NAME+$BUILD_NUMBER -r $GERRIT_REFSPEC"
+        if (fileExists 'logs/build_id')
         {
-         error 'Build ID not found'
+          def string_out = readFile('logs/build_id')
+          def build_id = string_out.replace("BUILD-ID=","")
+
+          if (build_id == null)
+          {
+           error 'Build ID not found'
+          }
+          if (args.ctest_tag == null)
+          {
+           error 'No ctest_tag found '
+          }
+          stage 'test'    
+          echo "Starting aurora test, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH ctest_tag:$args.ctest_tag"
+          //sh "aurora test -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.ctest_tag -n $args.num_instances -i $iter -l $build_id -A $args.test_args "
         }
-        if (args.ctest_tag == null)
-        {
-         error 'No ctest_tag found '
-        }
-        
-        stage 'test'    
-        echo "Starting aurora test, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH ctest_tag:$args.ctest_tag"
-        sh "aurora test -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.ctest_tag -n $args.num_instances -i $iter -l $build_id -A $args.test_args "
       }
 
       archiveArtifacts "$archive"
