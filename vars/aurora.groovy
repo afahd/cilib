004264497d4b6129dc9bd1018cc7eec496b692d6
Fahd Arshad <afahd@plumgrid.com>
2016-10-26 23:04:49 -0700

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -25,21 +25,21 @@ def call(body) {
       stage 'clone'
       dir('andromeda') 
       {
-        git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
+        //git branch: 'master', url: 'ssh://afahd@gerrit.plumgrid.com:29418/andromeda'
       } 
 
       withEnv(["PATH=/opt/plumgrid/google-cloud-sdk/bin/:$WORKSPACE/andromeda/gcloud/build:/opt/pg/scripts:$PATH"]) 
       {
-        sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..;'
+       // sh 'cd andromeda/gcloud/; mkdir -p build; cd build; cmake ..;'
 
         stage 'build'
         echo "Starting aurora build, project:$GERRIT_PROJECT, branch:$GERRIT_BRANCH refspec:$GERRIT_REFSPEC"
-        sh "aurora build -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $JOB_BASE_NAME+$BUILD_NUMBER -r $GERRIT_REFSPEC"
+       // sh "aurora build -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $JOB_BASE_NAME+$BUILD_NUMBER -r $GERRIT_REFSPEC"
         def string_out = readFile('logs/build_id')
         def build_id = string_out.replace("BUILD-ID=","")
 
         stage 'test'
-        sh "aurora test -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.ctest_tag -n $args.num_instances -i $iter -A $args.test_args -l $build_id "
+        sh "aurora test -l $build_id -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.ctest_tag -n $args.num_instances -i $iter -A $args.test_args"
       }
 
       archiveArtifacts "$args.archive"
