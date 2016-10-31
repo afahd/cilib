f7a02097c09a3f74c4cb47e4da53dac816010f2c
Fahd Arshad <afahd@plumgrid.com>
2016-10-31 01:57:41 -0700

    Update JenkinsLibrary.groovy

--- src/utils/JenkinsLibrary.groovy
+++ src/utils/JenkinsLibrary.groovy
@@ -102,6 +102,7 @@ def valueExist (def orignal_value, def argument)
 
 def checkGerritArguments()
 {
+    echo "$GERRIT_REFSPEC"
     if (GERRIT_REFSPEC == null)
     {
      error 'No GERRIT_REFSPEC found'
