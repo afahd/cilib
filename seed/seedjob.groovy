fcc5cb93e4730cd3ccbc9b4ab488936a3f2f70c5
Fahd Arshad <afahd@plumgrid.com>
2016-11-01 04:07:27 -0700

    Update seedjob.groovy

--- seed/seedjob.groovy
+++ seed/seedjob.groovy
@@ -17,10 +17,10 @@ checkout.consumeProcessOutput(sout, serr)
 checkout.waitFor()
 println "out> $sout err> $serr"
 
-folder(GERRIT_PROJECT) {
+folder("$GERRIT_PROJECT") {
     displayName("GERRIT_PROJECT")
     description("pipeplines for $GERRIT_PROJECT")
-    folder(GERRIT_PROJECT/GERRIT_BRANCH)
+    folder("$GERRIT_PROJECT/GERRIT_BRANCH")
     {
         displayName("$GERRIT_BRANCH")
         description("Pipelines for $GERRIT_PROJECT and branch: $GERRIT_BRANCH")
