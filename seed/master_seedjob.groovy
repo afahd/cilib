7a04a32069c9d56523e5c8e330591f6e1bfed4f2
irfanalis <irfans@plumgrid.com>
2016-11-15 10:46:03 +0500

    Update master_seedjob.groovy

--- seed/master_seedjob.groovy
+++ seed/master_seedjob.groovy
@@ -17,14 +17,16 @@ checkout.consumeProcessOutput(sout, serr)
 checkout.waitFor()
 println "out> $sout err> $serr"
 
+// Reading through the ci_enablied.list
 def ci_list = readFileFromWorkspace('ci_enabled.list')
 String[] split_file = ci_list.split(System.getProperty("line.separator"));
 
-def gerrit_url = "ssh://gerrit.plumgrid.com:29418/"
+// Creating a free style job named ci_seed_job
+freeStyleJob('master_ci_seed_job') {
+    // Adding the days for which to keep records of the builds
+    logRotator(15,-1,-1,-1)
 
-freeStyleJob('ci_seed_job') {
-    logRotator(-1, 10)
-
+    // Adding the Source code managment
     scm {
         git {
             remote {
@@ -36,7 +38,7 @@ freeStyleJob('ci_seed_job') {
     }
 
     triggers {
-        // Adding triggers to be used in review pipeline
+        // Adding gerrit triggers to be used
         gerrit
         {
             configure { GerritTrigger ->
@@ -44,25 +46,28 @@ freeStyleJob('ci_seed_job') {
                     'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.events.PluginCommentAddedContainsEvent' {
                         // Trigger event for comment added
                         // Adding runpipeline: <pipeline-name>
-                        commentAddedCommentContains(".*runpipeline: ci_seed_job")
+                        commentAddedCommentContains(".*runpipeline: master_ci_seed_job.*")
                     }
                     'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.events.PluginPatchsetCreatedEvent' {
                         // Using default values if jenkinsfile does not specify
                         excludeDrafts("True")
                         excludeTrivialRebase("False")
-                        excludeNoCodeChange("True")
+                        excludeNoCodeChange("False")
                     }
                 }
 
+                // Iterating over the projects in the ci_enabled.list
                 GerritTrigger << gerritProjects {
                     for (def line:split_file)
                     {
                         String[] line_split = line.split(" ")
                         repourl = line_split.getAt(0)
-                        repo = "${repourl}" - "${gerrit_url}"
+                        // Extracting the project name by removing the ssh://gerrit.plumgrid.com:29418/
+                        int idx = repourl.lastIndexOf('/')
+                        repo = repourl.substring(idx + 1)
                         branch = line_split.getAt(1)
                         println "${repo}, ${branch}"
-                        // Adding gerrit projects for gerrit trigger using GERRIT PROJECT and GERRIT_BRANCH
+                        // Adding gerrit project and gerrit branch for gerrit trigger from the ci_enabled.list
                         'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.GerritProject' {
                             compareType("PLAIN")
                             pattern(repo)
@@ -85,7 +90,7 @@ freeStyleJob('ci_seed_job') {
             }
         }
     }
-
+    // Adding dsl seed job
     steps {
         dsl{
             external('seed/seedjob.groovy')
