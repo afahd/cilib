cd34e68bd71be10100438c69a37e4f2ab3a83b5c
Fahd Arshad <afahd@plumgrid.com>
2016-11-29 11:05:17 -0800

    Update aurora.groovy

--- vars/aurora.groovy
+++ vars/aurora.groovy
@@ -78,7 +78,7 @@ def call(body) {
           try
           {
             stage 'Test'
-            echo "Starting aurora test, test_type:$args.test_type test_cmd:$args.test_cmd instance_id:$instance_id_cmd archive: $archive_logs"
+            echo "Starting aurora test, test_type:$args.test_type test_cmd:$args.test_cmd archive: $archive_logs"
             //sh "aurora test -p $GERRIT_PROJECT -b $GERRIT_BRANCH -t $args.test_type $test_args -c \"$args.test_cmd\" -n $instances $instance_id_cmd $archive_logs"
 
           } catch (err)
