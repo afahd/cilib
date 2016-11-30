24af0d92eadd8e2e040db12f722df8bae5495cc1
alirizw <alir@plumgrid.com>
2016-11-30 19:49:22 +0500

    Update external.groovy

--- vars/external.groovy
+++ vars/external.groovy
@@ -1,3 +1,5 @@
+// Importing Config Slurper using grapes
+@Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -5,10 +7,10 @@ def call(body) {
     body.delegate = config
     body()
     println "Config"
-    println config.archive1
     node('master') {
       git 'ssh://afahd@192.168.10.77:29418/phoenix.git'
-      def aurora = load('jenkins/jenkinsfiles/lint')
-        aurora(config)
-      }
+      pipeline_file = new File('jenkins/jenkinsfiles/lint').text
+      def pipeline_config = new ConfigSlurper().parse(pipeline_file)
+      println pipeline_config
     }
+}
