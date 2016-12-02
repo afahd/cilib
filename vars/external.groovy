842bb3fc783c26e5cf8eedc6c649585383169bdd
Fahd Arshad <afahd@plumgrid.com>
2016-12-02 07:21:00 -0800

    Update external.groovy

--- vars/external.groovy
+++ vars/external.groovy
@@ -7,10 +7,25 @@ def call(body) {
     body.delegate = config
     body()
     
+    //name = "pgui-falcon"
+    println config.name
+   
+    //project = "pgui"
+   
+    //type = "review"
+   
+    //branch = "master"
+   
+    //location = "ssh://afahd@192.168.10.77:29418/pgui.git"
+   
+    // trigger_path = "./"
+   
+    
     node('master') {
-      git 'ssh://afahd@192.168.10.77:29418/phoenix.git'
-        withEnv( ['TEST_PROJECT=testing123', 'TRIGGER_TYPE=yay']) {
-        load('jenkins/jenkinsfiles/lint')  
+      //git 'ssh://afahd@192.168.10.77:29418/phoenix.git'
+        withEnv( ['TEST_PROJECT=testing123', 'TRIGGER_TYPE=yay']) 
+        {
+        //load('jenkins/jenkinsfiles/lint')  
         }
     }
 }
